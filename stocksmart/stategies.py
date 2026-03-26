4de73604dc480a80f13a74548fb53aaaea7f5478
Xplorers-backend <brendan.soullens@gmail.com>
2026-03-26 16:56:38 -0600

    nose

--- stocksmart/stategies.py
+++ stocksmart/stategies.py
@@ -8,27 +8,27 @@ def trend_signals(ticker: yf.Ticker):
 
 
 def adx(df: pd.DataFrame, length: int = 14):
-        high = df["High"]
-        low = df["Low"]
-        close = df["Close"]
+    high = df["High"]
+    low = df["Low"]
+    close = df["Close"]
 
-        df["high_low"] = abs(high - low)
-        df["high_close"] = abs(high - close.shift(1))
-        df["low_close"] = abs(low - close.shift(1))
-        df["true_range"] = df[["high_low", "high_close", "low_close"]].max(axis=1)
+    df["high_low"] = abs(high - low)
+    df["high_close"] = abs(high - close.shift(1))
+    df["low_close"] = abs(low - close.shift(1))
+    df["true_range"] = df[["high_low", "high_close", "low_close"]].max(axis=1)
 
-        df["up_move"] = high - high.shift(1)
-        df["down_move"] = low.shift(1) - low
+    df["up_move"] = high - high.shift(1)
+    df["down_move"] = low.shift(1) - low
 
-        df["+dm"] = np.where((df["up_move"] > df["down_move"]) & (df["up_move"] > 0), df["up_move"], 0)
-        df["-dm"] = np.where((df["down_move"] > df["up_move"]) & (df["down_move"] > 0), df["down_move"], 0)
+    df["+dm"] = np.where((df["up_move"] > df["down_move"]) & (df["up_move"] > 0), df["up_move"], 0)
+    df["-dm"] = np.where((df["down_move"] > df["up_move"]) & (df["down_move"] > 0), df["down_move"], 0)
 
-        df["+di"] = 100 * (df["+dm"].ewm(span=length).mean() / df["true_range"].ewm(span=length).mean())
-        df["-di"] = 100 * (df["-dm"].ewm(span=length).mean() / df["true_range"].ewm(span=length).mean())
-        df["dx"] = 100 * abs(df["+di"] - df["-di"]) / (df["+di"] + df["-di"])
-        df["adx"] = df["dx"].ewm(span=length).mean()
+    df["+di"] = 100 * (df["+dm"].ewm(span=length).mean() / df["true_range"].ewm(span=length).mean())
+    df["-di"] = 100 * (df["-dm"].ewm(span=length).mean() / df["true_range"].ewm(span=length).mean())
+    df["dx"] = 100 * abs(df["+di"] - df["-di"]) / (df["+di"] + df["-di"])
+    df["adx"] = df["dx"].ewm(span=length).mean()
 
-        return df[["adx", "+di", "-di"]]
+    return df[["adx", "+di", "-di"]]
 
         
 
